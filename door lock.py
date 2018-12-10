9a4dd43633b20b3506c7c623e6405c1e5cab941b
Eric Mailman <emailman@dtcc.edu>
2018-12-10 18:12:22 -0500

    door lock program, version 2, added lights

--- door lock.py
+++ door lock.py
@@ -11,15 +11,19 @@ def get_code():
         if sensor.input('Button_1') == 'pressed':
             code.append('1')
             i += 1
+            print(i, code)
         elif sensor.input('Button_2') == 'pressed':
             code.append('2')
             i += 1
+            print(i, code)
         elif sensor.input('Button_3') == 'pressed':
             code.append('3')
             i += 1
+            print(i, code)
         elif sensor.input('Button_4') == 'pressed':
             code.append('4')
             i += 1
+            print(i, code)
         else:
             sleep(.1)
 
@@ -29,14 +33,26 @@ def get_code():
 def main():
     lock_code = get_code()
     print('lock code =', lock_code)
+    sensor.output('bled', 'on')
 
-    unlock_code = get_code()
-    print('unlock code =', unlock_code)
+    while True:
+        if sensor.input('light') == 'on':
+            unlock_code = get_code()
+            print('unlock code =', unlock_code)
 
-    if lock_code == unlock_code:
-        print('Unlocked')
-    else:
-        print('Unlock Failed')
+            if lock_code == unlock_code:
+                print('Unlocked')
+                sensor.output('gled', 'on')
+                sleep(2)
+                sensor.output('gled', 'off')
+            else:
+                print('Unlock Failed')
+                sensor.output('rled', 'on')
+                sleep(2)
+                sensor.output('rled', 'off')
+        else:
+            sensor.output('bled', 'off')
+            break
 
 
 main()
