e67278cd4ca7fbe318af7047defd634bf830b0a5
Eric Mailman <eric.mailman@yahoo.com>
2018-12-19 14:09:55 -0500

    Added second window for outdoor display

--- Electronic Scoreboard.py
+++ Electronic Scoreboard.py
@@ -1,20 +1,45 @@
 from guizero import *
 
+txt_home_win = None
+txt_visitors_win = None
 
-def score(team):
+
+def score(team, team_win):
     goals = int(team.value)
     team.value = goals + 1
+    team_win.value = team.value
 
 
 def clear(team1, team2):
     team1.value = 0
     team2.value = 0
+    txt_home_win.value = 0
+    txt_visitors_win.value = 0
 
 
 def main():
-    app = App(title='Score Board', height=250, width=250, layout='grid')
+    global txt_home_win, txt_visitors_win
 
-    Text(app, text=' ' * 14, size=6, grid=[0, 0])
+    app = App(title='Score Board Console', height=250, width=280, layout='grid')
+    win = Window(app, title='Score Board', height=120, width=280, layout='grid')
+
+    # Outdoor board
+    Text(win, text=' ' * 20, size=6, grid=[0, 0])
+
+    Text(win, text='BLUE HENS LACROSSE', color='blue', grid=[1, 1, 3, 1])
+
+    Text(win, size=6, grid=[0, 2])
+
+    Text(win, text='HOME', color='green', grid=[1, 3])
+    txt_home_win = Text(win, text='0', color='green', grid=[1, 4])
+    Text(win, size=4, grid=[1, 5])
+
+    Text(win, text='VISITORS', color='red', grid=[3, 3])
+    txt_visitors_win = Text(win, text='0', color='red', grid=[3, 4])
+    Text(win, size=4, grid=[3, 5])
+
+    # Console
+    Text(app, text=' ' * 20, size=6, grid=[0, 0])
 
     Text(app, text='BLUE HENS LACROSSE', color='blue', grid=[1, 1, 3, 1])
 
@@ -24,14 +49,14 @@ def main():
     txt_home = Text(app, text='0', color='green', grid=[1, 4])
     Text(app, size=4, grid=[1, 5])
     btn_home = PushButton(app, text='SCORE',
-                          command=score, args=[txt_home], grid=[1, 6])
+                          command=score, args=[txt_home, txt_home_win], grid=[1, 6])
     btn_home.bg = 'green'
 
     Text(app, text='VISITORS', color='red', grid=[3, 3])
     txt_visitors = Text(app, text='0', color='red', grid=[3, 4])
     Text(app, size=4, grid=[3, 5])
     btn_visitors = PushButton(app, text='SCORE',
-                              command=score, args=[txt_visitors], grid=[3, 6])
+                              command=score, args=[txt_visitors, txt_visitors_win], grid=[3, 6])
     btn_visitors.bg = 'red'
 
     Text(app, grid=[0, 7])
