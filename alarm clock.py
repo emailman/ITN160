2fced69f79ebed605ba7231904f5c6ba6a6ef17b
Eric Mailman <eric.mailman@yahoo.com>
2018-12-22 11:37:26 -0500

    Added alarm clock app

---
+++ alarm clock.py
@@ -0,0 +1,73 @@
+import physense_emu
+import time
+
+# create sensor
+sensor = physense_emu.Sensor()
+
+# flags for alarm snooze and alarm enabled
+alarm_enabled = False
+snooze_enabled = False
+
+
+def alarm_check():
+    global alarm_enabled
+    if sensor.input('Button_3') == 'pressed':
+        # toggle the flag
+        alarm_enabled = not alarm_enabled
+
+    # set green LED based on the flag
+    if alarm_enabled:
+        sensor.output('gled', 'on')
+    else:
+        sensor.output('gled', 'off')
+
+
+def snooze_check():
+    global snooze_enabled
+    if sensor.input('Button_4') == 'pressed':
+        # toggle the flag
+        snooze_enabled = not snooze_enabled
+
+    # set blue LED based on the flag
+    if snooze_enabled:
+        sensor.output('bled', 'on')
+    else:
+        sensor.output('bled', 'off')
+
+
+def print_time():
+    current_time = time.localtime(time.time())
+    print(time.strftime("%H:%M:%S %p", current_time))
+
+
+def main():
+    global snooze_enabled
+
+    while True:
+
+        alarm_check()
+        snooze_check()
+        print_time()
+
+        # turn on the red LED and alarm if light is on,
+        # alarm is enabled and the snooze is disabled
+        if sensor.input("light") == "on" \
+                and alarm_enabled and not snooze_enabled:
+            sensor.output('rled', 'on')
+            sensor.output("buzz", "play")
+        else:
+            time.sleep(1)
+            sensor.output('rled', 'off')
+
+        # check if the snooze is enabled
+        if snooze_enabled:
+            # shut the alarm off for 10 seconds
+            for i in range(10):
+                print_time()
+                time.sleep(1)
+
+            # disable the snooze
+            snooze_enabled = False
+
+
+main()
